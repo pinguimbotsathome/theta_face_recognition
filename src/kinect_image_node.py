2738c2bc7d839f1e39f54dfb66d23366d4dfb957
HerickDallAgnol <herick.nunes@ufpel.edu.br>
2023-08-21 17:19:39 -0300

    first commit

---
+++ src/kinect_image_node.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+import rospy
+import cv2
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+def image_callback(data):
+    original_image_pub = rospy.Publisher("bridge/original_image", Image, queue_size=1) 
+
+    bridge = CvBridge()
+    try:
+        cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
+        original_image_msg = bridge.cv2_to_imgmsg(cv_image, "bgr8")  
+        original_image_pub.publish(original_image_msg)
+        
+    except CvBridgeError as e:
+        print(e)
+
+rospy.init_node('kinect_image_node', anonymous=True)  
+
+rospy.Subscriber("camera/rgb/image_raw", Image, image_callback, queue_size=1) 
+rospy.spin()
