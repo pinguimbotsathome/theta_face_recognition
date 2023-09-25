7dd3b053d70792e8325d83a23f3eb527929a45ff
HerickDallagnol <herickdalll@gmail.com>
2023-09-25 15:13:22 -0300

    mudança para obter a imagem pela webcam e nao pelo kinect

--- src/trainer_node.py
+++ src/trainer_node.py
@@ -1,19 +1,23 @@
 #! /usr/bin/env python3
 import rospy
 import cv2
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
 import os.path
 
-def detect(data):
-    bridge = CvBridge()
-    cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
-
-    if os.path.exists('/home/dallagnol/work_ws/src/theta_face_recognition/dataset/Comparador.png'):
-        return
-    else:
-        cv2.imwrite("/home/dallagnol/work_ws/src/theta_face_recognition/dataset/Comparador.png", cv_image)
-    
 rospy.init_node('trainer_node', anonymous=True)
-rospy.Subscriber("bridge/original_image", Image, detect, queue_size=1)
-rospy.spin()
+
+def detection():
+    webcam = cv2.VideoCapture(0)
+    if webcam.isOpened():
+        validacao, frame = webcam.read()
+        while validacao:
+            validacao, frame = webcam.read()
+            if os.path.exists('/home/dallagnol/work_ws/src/theta_face_recognition/dataset/Comparador.png'):
+                break
+            else:   
+                cv2.imwrite("/home/dallagnol/work_ws/src/theta_face_recognition/dataset/Comparador.png", frame)
+
+if __name__ == "__main__":
+    try:
+        detection()
+    except rospy.ROSInterruptException:
+        pass 
