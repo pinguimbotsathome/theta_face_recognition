0436715e68f375f0468a7c2cb1fc10f595b9f455
HerickDallagnol <herickdalll@gmail.com>
2023-09-25 15:44:33 -0300

    correção recognition

--- src/face_recognition_node.py
+++ src/face_recognition_node.py
@@ -2,34 +2,37 @@
 import rospy
 import cv2
 import face_recognition as fr
-import imageio
 
 def recogniton():
-    imgOperador = imageio.imread("/home/dallagnol/work_ws/src/theta_face_recognition/dataset/Operador.png")
-    imgOperador = cv2.cvtColor(imgOperador,cv2.COLOR_BGR2RGB)
+    imgOperador = fr.load_image_file("/home/dallagnol/work_ws/src/theta_face_recognition/dataset/Operador.png")
+    imgOperador = cv2.cvtColor(imgOperador, cv2.COLOR_BGR2RGB)
 
-    imgComparador = imageio.imread("/home/dallagnol/work_ws/src/theta_face_recognition/dataset/Comparador.png")
-    imgComparador = cv2.cvtColor(imgComparador,cv2.COLOR_BGR2RGB)
+    imgComparador = fr.load_image_file("/home/dallagnol/work_ws/src/theta_face_recognition/dataset/Comparador.png")
+    imgComparador = cv2.cvtColor(imgComparador, cv2.COLOR_BGR2RGB)
 
-    faceLoc = fr.face_locations(imgOperador)[0]
-    cv2.rectangle(imgOperador,(faceLoc[3],faceLoc[0]),(faceLoc[1],faceLoc[2]),(0,255,0),2)
+    faceLoc = fr.face_locations(imgComparador)[0]
+    cv2.rectangle(imgComparador, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (0, 255, 0), 2)
 
     encondeOperador = fr.face_encodings(imgOperador)[0]
     encodeComparador = fr.face_encodings(imgComparador)[0]
 
-    comparacao = fr.compare_faces([encondeOperador],encodeComparador)
-    #coordenadas = fr.face_distance([encondeOperador],encodeComparador)
+    comparacao = fr.compare_faces([encondeOperador], encodeComparador)
+    distancia = fr.face_distance([encondeOperador], encodeComparador)
+    x1, y1, x2, y2 = faceLoc
 
-    rospy.loginfo("Results:")
-    rospy.loginfo(comparacao)
+    rospy.loginfo('\nResultados:')
+    rospy.loginfo(f'Comparação: {comparacao}')
+    rospy.loginfo(f'Distância: {distancia}')
 
-    cv2.imshow('Operador',imgOperador)
-    cv2.imshow('Comparador',imgComparador)
-    cv2.waitKey(0)
+    rospy.loginfo(f'Coordenadas: x1={x1}, y1={y1}, x2={x2}, y2={y2}')
+
+    cv2.imshow('Operador', imgOperador)
+    cv2.imshow('Comparador', imgComparador)
+    cv2.waitKey()
 
 if __name__ == '__main__':
     try:
         rospy.init_node('image_receive_node', anonymous=True)
         recogniton()
     except rospy.ROSInterruptException:
-        pass               
+        pass
