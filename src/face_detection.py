1d6d592876bb04ae9cd713ab15edce8449b6e6d9
HerickDallagnol <herickdalll@gmail.com>
2023-09-27 17:26:02 -0300

    modificação para utilizar o recognition no mesmo codigo

--- src/face_detection.py
+++ src/face_detection.py
@@ -2,13 +2,11 @@
 import rospkg
 import rospy
 import cv2
+import face_recognition as fr
 from sensor_msgs.msg import Image
 from std_msgs.msg import Empty
 from cv_bridge import CvBridge
 import os.path
-
-
-rospy.init_node('face_detection_node', anonymous=True)
 
 PACK_DIR = rospkg.RosPack().get_path("theta_face_recognition")
 OPERADOR_DIR = os.path.join(PACK_DIR, "dataset/operador.png")
@@ -36,11 +34,42 @@ def comparador(data):
     else:
         cv2.imwrite(COMPARADOR_DIR, cv_image)
 
+def recogniton():
+    imgOperador = fr.load_image_file(OPERADOR_DIR)
+    imgOperador = cv2.cvtColor(imgOperador, cv2.COLOR_BGR2RGB)
+
+    imgComparador = fr.load_image_file(COMPARADOR_DIR)
+    imgComparador = cv2.cvtColor(imgComparador, cv2.COLOR_BGR2RGB)
+
+    faceLoc = fr.face_locations(imgComparador)[0]
+    cv2.rectangle(imgComparador, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (0, 255, 0), 2)
+
+    encondeOperador = fr.face_encodings(imgOperador)[0]
+    encodeComparador = fr.face_encodings(imgComparador)[0]
+
+    comparacao = fr.compare_faces([encondeOperador], encodeComparador)
+    distancia = fr.face_distance([encondeOperador], encodeComparador)
+    x1, y1, x2, y2 = faceLoc
+
+    rospy.loginfo('\nResultados:')
+    rospy.loginfo(f'Comparação: {comparacao}')
+    rospy.loginfo(f'Distância: {distancia}')
+
+    rospy.loginfo(f'Coordenadas: x1={x1}, y1={y1}, x2={x2}, y2={y2}')
+
+    cv2.imshow('Operador', imgOperador)
+    cv2.imshow('Comparador', imgComparador)
+    cv2.waitKey()
+
 if __name__ == '__main__':
     try:
-        subOperador = rospy.Subscriber('/operador_take', Empty, operador)
-        subComparador = rospy.Subscriber('/comparador_take', Empty, comparador)
+        rospy.init_node('face_detection_node', anonymous=True)
         rospy.Subscriber("bridge/original_image", Image, comparador, queue_size=1)
+
+        sub_operador = rospy.Subscriber('/face_detection/operador_take', Empty, operador)
+        sub_comparador = rospy.Subscriber('/face_detection/comparador_take', Empty, comparador)
+        sub_recognition = rospy.Subscriber('/face_detection/recognition', Empty,recogniton)
+        
         while not rospy.is_shutdown():
             pass
     except rospy.ROSInterruptException:
